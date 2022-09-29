ebb56761b128ee8ceff5b702b9f343847831979a
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-09-29 17:49:15 +0300

    Add project files.

---
+++ 08. Graduation pt.2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _08._Graduation_pt._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+
+            int grades = 1;
+
+            double sum = 0.0;
+            int cou = 0;
+
+            while (grades <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                cou++;
+                grade += grade;
+                double ave = grade / cou;
+
+                if (ave < 4.00)
+                {
+                    Console.WriteLine($"{name}  has been excluded at {grdes} grade");
+                }
+
+                cou++;
+                grade += grade;
+                double ave = grade / cou;
+
+                Console.WriteLine($"{name}");
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ 09. Moving/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int w = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+            int H = int.Parse(Console.ReadLine());
+
+            int place = w * h * H;
+
+            int sum = 0;
+
+            string input = Console.ReadLine();
+
+            while (input != "Done")
+            {
+                int num = int.Parse(input);
+                sum += num;
+
+                if (place < sum)
+                {
+                    int d = sum - place;
+                    Console.WriteLine($"No more free space! You need {d} Cubic meters more.");
+                    break;
+
+                }
+
+               
+
+                input = Console.ReadLine();
+
+
+                if (input == "Done")
+                {
+                    Console.WriteLine($"{ place - sum} Cubic meters left.");
+                }
+
+            }
+
+
+        }
+    }
+}
